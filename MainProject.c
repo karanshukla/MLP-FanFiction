430409c717c9dab033a190b46cf2f150a05e6cf5
RiseBeaver <c.chih2@gmail.com>
2013-03-24 18:04:47 -0300

    Update MainProject.c

--- MainProject.c
+++ MainProject.c
@@ -21,41 +21,51 @@ else if (num==4)    colour = 'g';
 return colour;
 }
 
-void printBoard(){
+void compileBoard (char arr[][36], int ROW, int COL){	//editted Rachel's original one
 
-srand((unsigned)time(NULL));
-
-int col, row, A[ROW+1][COL+1];
-
-for (col=0; col<COL+1; col++){
-    for (row=0; row<ROW+1; row++){
-        if (row==0){
-            if (col==COL)
-                printf ("    ");
-            else if ((COL-col)>10)
-                printf ("%3c ", ((COL-col) + '9' -3));
-            else
-            printf ("%3d ", COL-col-1);
-        }
-        else if (col==COL){
-            if (row>10)
-                printf ("%c ", (row + '9'-3));
-            else
-            printf ("%d ", row-1);
-        }
-        else {A[row][col] = rand()%4+1;
-        printf ("%c ", NumToColour(A[row][col]));
-        }
+int i, j;
+srand ((unsigned)time(NULL));
+for (i=0;i<ROW;i++){
+    for (j=0;j<COL;j++){
+        arr[i][j]=NumToColour(rand()%4+1);
     }
-printf ("\n");
 }
 return;
 }
+
+void printBoard (char arr[][36], int ROW, int COL){
+
+int col, row;   //row and col are inverted because of some weird logic going on here
+
+for (col=0; col<COL+1; col++){
+    for (row=-1; row<ROW; row++){
+        if (row==-1){        //for y-coordinates
+            if (col==COL)       //for the bottom left corner space
+                printf ("    ");
+            else if ((COL-col)>10)  //for coordinates bigger than 10
+                printf ("%3c ", ((COL-col) + '9' -3));
+            else
+            printf ("%3d ", COL-col-1); //inverting coordinates from column number in array
+        }
+        else if (col==COL){     //for the x-coorfinates
+            if (row>9)     //for coordinates bigger than 10
+                printf ("%c ", (row + '9'-2));
+            else
+            printf ("%d ", row);
+        }
+        else
+        printf ("%c ", arr[col][row]);
+    }
+printf ("\n");
+}
+}
+
 
 /* below are the functions I did I have not tested them yet and I still need to add more comments--- Rachel*/
 char compileBoard(int rows, int cols){/* this function creates an arry of characters (arr) ith the specified size. it is formatted with the colums first (x coordanite) and the rows second (y cordinate)
                                          it takes as paramers the row and colom size
                                          the character array corisponds to a color.*/
+                                         //I've fixed some stuff in this function, scroll up. -Beaver
     int i,j, temp;
     char arr[col][row];
     srand ((unsigned)time(NULL));
