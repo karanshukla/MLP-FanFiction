7dd57a849c1001e502048faed14e635947440a3a
karanshukla <karan_shukla@live.com>
2013-04-10 18:00:58 -0300

    Update ReturnScoreAss.c

--- ReturnScoreAss.c
+++ ReturnScoreAss.c
@@ -7,15 +7,13 @@
 int CalculateScore (char arr[][36], int ROW, int COL){
     int i, j;
     int fscore = 0; //score for function
-    int tempscore; //dummy variable
     for (i = 0; i < ROW; i++) //Loop through Array
         for (j = 0; j < COL;j++){
-            tempscore = fscore
             if (arr[i][j] == '0')
                 fscore++; //Increases score!
-            else if (tempscore == fscore)
-                return -1;
-       
         }
+    if (fscore == 0)
+        return -1;
+    else
     return fscore*fscore; //ITS HIP TO BE SQUARE
 }
