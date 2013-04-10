e6c296d2ff2fc8d88708eb878fd63dbe7ba119ac
karanshukla <karan_shukla@live.com>
2013-04-10 17:21:43 -0300

    Update ReturnScoreAss.c

--- ReturnScoreAss.c
+++ ReturnScoreAss.c
@@ -9,7 +9,7 @@ int CalculateScore (char arr[][36], int ROW, int COL){
     int score = 0;
     for (i = 0; i < ROW; i++) //Loop through Array
         for (j = 0; j < COL;j++){
-            if (arr[i][j] == 0)
+            if (arr[i][j] == '0')
                 score++; //Increases score!
         }
     return score*score; //ITS HIP TO BE SQUARE
